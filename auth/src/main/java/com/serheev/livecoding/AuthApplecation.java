76030c92c23e345d903249eb2bb90dfc60e24410
Serheev <yserheev@gmail.com>
2020-02-08 12:46:43 +0200

    features: add AuthApplication file

---
+++ auth/src/main/java/com/serheev/livecoding/AuthApplecation.java
@@ -0,0 +1,9 @@
+package com.serheev.livecoding;
+
+import org.springframework.boot.SpringApplication;
+
+public class AuthApplecation {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplecation.class, args);
+    }
+}
