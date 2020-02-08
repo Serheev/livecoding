3e47f444b77fd9e369943e97fba95f5c34c71d11
Serheev <yserheev@gmail.com>
2020-02-08 13:09:22 +0200

    features: add application-it.yml file

--- app/src/test/java/com/serheev/livecoding/LivecodingApplicationTests.java
+++ app/src/test/java/com/serheev/livecoding/LivecodingApplicationTests.java
@@ -2,7 +2,9 @@ package com.serheev.livecoding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(classes=LivecodingApplication.class)
 class LivecodingApplicationTests {
 
