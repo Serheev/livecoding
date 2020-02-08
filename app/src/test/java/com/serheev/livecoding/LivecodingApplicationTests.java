4210e6f872ebb88dd524a27ba915b84cc73acae3
Serheev <yserheev@gmail.com>
2020-02-08 12:17:09 +0200

    features: create microservices

--- app/src/test/java/com/serheev/livecoding/LivecodingApplicationTests.java
+++ app/src/test/java/com/serheev/livecoding/LivecodingApplicationTests.java
@@ -3,7 +3,7 @@ package com.serheev.livecoding;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=LivecodingApplication.class)
 class LivecodingApplicationTests {
 
     @Test
